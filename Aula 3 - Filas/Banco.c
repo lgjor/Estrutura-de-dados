4df82f9642d6de5dd57977731ab48707e4035c88
Lucas Godoy <lucasgodoyjor@gmail.com>
2025-01-17 09:51:52 -0300

    Fila em C

---
+++ Aula 3 - Filas/Banco.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "Fila.h"
+
+int main(int argc, char *argv[]){
+
+    struct Fila f;
+    int elemento;
+    create(&f); // Cria a fila vazia
+
+    // Inserções
+    insert(&f, 345);
+    insert(&f, 917);
+    insert(&f, 14);
+    insert(&f, 862);
+    insert(&f, 5763);
+    insert(&f, 2);
+
+    printf("Bem vindos ao IsiBank!\n");
+
+    printf("Atendimento:\n");
+
+    while (!isEmpty(f)){
+        elemento = remove(&f);
+        printf("Número da senha a ser removida %d\n", elemento);
+    }
+    return 0;
+}

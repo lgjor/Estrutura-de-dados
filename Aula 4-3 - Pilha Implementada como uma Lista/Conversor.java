b2ae27522c8ef88cbc66aa274a485db11179383f
Lucas Godoy <lucasgodoyjor@gmail.com>
2025-01-22 09:45:37 -0300

    PilhaDinamica

---
+++ Aula 4-3 - Pilha Implementada como uma Lista/Conversor.java
@@ -0,0 +1,24 @@
+public class Conversor{
+    public static void main(String[] args){
+        int numero = Integer.parseInt(args[0]);
+        PilhaDinamica p = new PilhaDinamica();
+
+        int resto;
+        
+        // fase 1 - empilhamento dos restos
+        while(numero != 0){
+            resto = numero % 2; // pego o resto da divisão deste número por 2 (vale 0 ou 1)
+            p.push(resto);      // armazeno na pilha
+            numero = numero / 2;// gero um novo número, resultado da divisão dele por 2
+        }
+
+        // fase 2 - exibição dos restos na ordem correta
+        while(!p.isEmpty()){
+            resto = p.pop(); // desempilho
+            System.out.print(resto);
+        }
+    System.out.print("Fim do programa");
+    }
+}
+
+// 172 na base 10 é igual a 10101100 na base 2
